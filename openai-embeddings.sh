d9b8117ed71c550a9c7775d50407223b369a25aa
Michael Wechner <michael.wechner@wyona.com>
2022-02-11 23:08:13 +0100

    documentation link added

--- openai-embeddings.sh
+++ openai-embeddings.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Weaviate documentation: https://www.semi.technology/developers/weaviate/current/modules/text2vec-openai.html
+# Get OpenAPI key: https://beta.openai.com/account/api-keys
+
 curl https://api.openai.com/v1/engines/text-search-ada-query-001/embeddings \
   -k -X POST \
   -H "Authorization: Bearer YOUR_API_KEY" \
