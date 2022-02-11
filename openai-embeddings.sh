0bdfad929f19850b1563790304ff201028a08662
Michael Wechner <michael.wechner@wyona.com>
2022-02-11 23:31:20 +0100

    similarity added

--- openai-embeddings.sh
+++ openai-embeddings.sh
@@ -3,7 +3,8 @@
 # Weaviate documentation: https://www.semi.technology/developers/weaviate/current/modules/text2vec-openai.html
 # Get OpenAPI key: https://beta.openai.com/account/api-keys
 
-curl https://api.openai.com/v1/engines/text-search-ada-query-001/embeddings \
+#curl https://api.openai.com/v1/engines/text-search-ada-query-001/embeddings \
+curl https://api.openai.com/v1/engines/text-similarity-ada-001/embeddings \
   -k -X POST \
   -H "Authorization: Bearer YOUR_API_KEY" \
   -H "Content-Type: application/json" \
