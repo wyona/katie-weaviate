4c3125261d58826f494e87273821ce109c66eefd
Michael Wechner <michael.wechner@wyona.com>
2023-01-24 12:32:59 +0100

    interface moved

--- katie-weaviate-connector/src/main/java/org/wyona/webapp/controllers/v2/KatieWeaviateConnectorController.java
+++ katie-weaviate-connector/src/main/java/org/wyona/webapp/controllers/v2/KatieWeaviateConnectorController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.wyona.webapp.controllers.KatieConnectorController;
 import org.wyona.webapp.models.katie.Answer;
 import org.wyona.webapp.models.katie.Domain;
 import org.wyona.webapp.models.katie.QnA;
