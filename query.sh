b8482c145bf5da456c76f1de3772c00670f8793b
Michael Wechner <michael.wechner@wyona.com>
2023-04-25 12:48:32 +0200

    query to get vectors

---
+++ query.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+BASE_URL=http://0.0.0.0:8080/v1/graphql
+#BASE_URL=https://weaviate.ukatie.com/v1/graphql
+
+echo "Query Weaviate ($BASE_URL) ..."
+
+curl -k -X POST -H "Content-Type: application/json" -d '{"query":"{Get {Question { question _additional { vector }}}}"}' $BASE_URL
