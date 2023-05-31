90ac79b3d40fd8ce0c8d3f4fc177ab1996b4f196
Michael Wechner <michael.wechner@wyona.com>
2023-05-31 09:01:30 +0200

    OpenAIDocument example added

--- query.sh
+++ query.sh
@@ -6,3 +6,5 @@ BASE_URL=http://0.0.0.0:8080/v1/graphql
 echo "Query Weaviate ($BASE_URL) ..."
 
 curl -k -X POST -H "Content-Type: application/json" -d '{"query":"{Get {Question { question _additional { vector }}}}"}' $BASE_URL
+
+#curl -k -X POST -H "Content-Type: application/json" -d '{"query":"{Get {OpenAIDocument { text _additional { vector }}}}"}' $BASE_URL
